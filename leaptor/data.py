49129d2b25bde23fdc5f2539fc12211d9d192e8a
Philipp Meister <philipp.meister@hacon.de>
2024-08-16 16:05:24 +0200

    more features: select employee via double click

--- leaptor/data.py
+++ leaptor/data.py
@@ -7,25 +7,47 @@ class Employee:
         self.salaryGroup = salaryGroup
         self.performanceGroup = performanceGroup
 
-
+    def copy_values(self,oth):
+        self.name = oth.name
+        self.jobDesc = oth.jobDesc
+        self.salaryGroup = oth.salaryGroup
+        self.performanceGroup = oth.performanceGroup
 
 class EmployeeList:
     def __init__(self):
         self.list = []
 
+    def find_employee(self, name):
+        index = [i for i, listEmployee in enumerate(self.list) if listEmployee.name == name]
+        if len(index) > 0:
+            return self.list[index.pop()]
+        return None
+
     def add_employee(self, employee):
-        self.list.append(employee)
+        listEmployee = self.find_employee(employee.name)
+        if listEmployee:
+            listEmployee.copy_values(employee)
+        else:
+            self.list.append(employee)
 
     def get_employees(self):
         return self.list
 
-    def save_file(self, filename):
-        with open(filename, 'w') as file:
-            json.dump(self.get_employees(), file)
+    def clear(self):
+        self.list.clear()
 
+    def save_file(self, filename):
+    
+        out = []
+        for employee in self.get_employees():
+            out.append( (employee.name, employee.jobDesc, employee.salaryGroup, employee.performanceGroup))
+        with open(filename, 'w') as file:
+            json.dump(out, file)
+    
     def load_file(self, filename):
         with open(filename, 'r') as file:
             employees = json.load(file)
+            self.clear()
             for name, jobDesc, salaryGroup, performanceGroup in employees:
                 employee = Employee(name, jobDesc, salaryGroup, performanceGroup)
                 self.add_employee(employee)
--- leaptor/gui.py
+++ leaptor/gui.py
@@ -20,12 +20,22 @@ class LeaptorGUI:
 
         self.label_salary = tk.Label(self.root, text="Gehaltsgruppe:")
         self.label_salary.grid(row=2, column=0)
-        self.entry_salary = tk.Entry(self.root)
+
+        self.salary_value = tk.StringVar(self.root)
+        self.salary_values = ["1", "2", "3", "4", "5", "6", "7", "8"]
+        self.salary_default = 3
+        self.salary_value.set(self.salary_values[self.salary_default])
+        self.entry_salary = tk.OptionMenu(self.root, self.salary_value, *self.salary_values)
         self.entry_salary.grid(row=2, column=1)
 
         self.label_performance = tk.Label(self.root, text="Leistungsbewertung")
         self.label_performance.grid(row=3, column=0)
-        self.entry_performance = tk.Entry(self.root)
+        
+        self.performance_value = tk.StringVar(self.root)
+        self.performance_values = ["d","c","b3","b2","b1","a"]
+        self.performance_default = 3
+        self.performance_value.set(self.performance_values[self.performance_default])
+        self.entry_performance = tk.OptionMenu(self.root, self.performance_value, *self.performance_values)
         self.entry_performance.grid(row=3, column=1)
 
         self.button_save_employee = tk.Button(self.root, text="Mitarbeiter speichern", command=self.save_employee)
@@ -33,52 +43,78 @@ class LeaptorGUI:
 
         self.employee_list_box = tk.Listbox(self.root, width=50, height=25)
         self.employee_list_box.grid(row=5, columnspan=2, column=0)
+        self.employee_list_box.bind("<Double-1>", self.on_double_click)
 
-        self.button_load_file = tk.Button(self.root, text="Gehälter von Datei laden", command=self.load_file)
+        self.button_load_file = tk.Button(self.root, text="Mitarbeiter aus Datei lesen", command=self.load_file)
         self.button_load_file.grid(row=6, column=0)
-        self.button_save_file = tk.Button(self.root, text="Gehälter in Datei speichern", command=self.save_file)
+        self.button_save_file = tk.Button(self.root, text="Mitarbeiter in Datei speichern", command=self.save_file)
         self.button_save_file.grid(row=6, column=1)
+
 
     def clear_entries(self):
         self.entry_name.delete(0, tk.END)
         self.entry_jobDesc.delete(0, tk.END)
-        self.entry_salary.delete(0, tk.END)
-        self.entry_performace.delete(0, tk.END)
+        self.salary_value.set(self.salary_values[self.salary_default])
+        self.performance_value.set(self.performance_values[self.performance_default])
+
+    def get_current_selection(self):
+        selected = self.employee_list_box.curselection()
+        if selected:
+            text = self.employee_list_box.get(selected[0])
+            name = text.split("-")[0]
+            return self.employees.find_employee(name)
+
+
+    def on_double_click(self, event):
+        employee = self.get_current_selection()
+        if employee:
+            self.entry_name.delete(0, tk.END)
+            self.entry_name.insert(0, employee.name)
+            self.entry_jobDesc.delete(0, tk.END)
+            self.entry_jobDesc.insert(0, employee.jobDesc)
+            self.performance_value.set(employee.performanceGroup)
+            self.salary_value.set(employee.salaryGroup)
+        else:
+            messagebox.showerror("Fehler", "Das Hervorheben eines Mitarbeiters ist unerwartet fehlgeschlaten")
 
     def refresh_employee_list_box(self):
         self.employee_list_box.delete(0, tk.END)
         for employee in self.employees.get_employees():
-           self.employee_list_box.insert(tk.END, f"{employee.name}-{employee.jobDesc}:{employee.salaryGroup}{employee.performanceGroup}");
-
+           self.employee_list_box.insert(tk.END, f"{employee.name}-'{employee.jobDesc}':{employee.salaryGroup}{employee.performanceGroup}");
 
     def save_employee(self):
         name = self.entry_name.get()
         jobDesc = self.entry_jobDesc.get()
-        salaryGroup = self.entry_salary.get()
-        performance = self.entry_performance.get()
+        salaryGroup = self.salary_value.get()
+        performance = self.performance_value.get()
 
         employee = Employee(name, jobDesc, salaryGroup, performance)
         self.employees.add_employee(employee);
         self.refresh_employee_list_box()
         self.clear_entries()
 
-
     def save_file(self):
-        self.employees.save_file("employee.json")
+        try:
+            file_name = "employee.json"
+            self.employees.save_file(file_name)
+            messagebox.showinfo( "Mitarbeiter gespeichert", f"Mitarbeiter erfolgreich in der Datei {file_name} gespeichert!")
+        except Exception as e:
+            messagebox.showerror( "Schreibefehler", f"Es ist ein unerwarteter Fehler aufgetreten: {e}")
 
     def load_file(self):
-        clear_entries()
-        self.employee_list_box.delete(0, tk.END) # todo später abmischen
+        try:
+            file_name = "employee.json"
+            self.employee_list_box.delete(0, tk.END) # todo später abmischen
 
-        self.employees.load_file("employee.json")
+            self.employees.load_file(file_name)
+            self.refresh_employee_list_box()
+            messagebox.showinfo( "Mitarbeiter geladen", f"Mitarbeiter erfolgreich aus der Datei {file_name} geladen!")
+        except FileNotFoundError:
+            messagebox.showerror( "Ladefehler", f"Das Laden der Datei {file_name} ist fehlgeschlagen. Die Datei ist nicht vorhanden!")
+        except Exception as e:
+            messagebox.showerror( "Ladefehler", f"Es ist ein unerwarteter Fehler aufgetreten: {e}")
+
 
     def run(self):
         self.root.mainloop()
-
-
-
-
-
-
-
 
